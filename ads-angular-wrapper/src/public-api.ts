c7de86fdc4b2055f5f2c3b1972d99f3fda4dac83
Sackah <yeitsam5@gmail.com>
2024-03-29 11:04:15 +0000

    refactor: removed local version of ads

--- ads-angular-wrapper/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ads-angular-wrapper
- */
-
-export * from './lib/proxy';
-export * from './lib/ads-angular-wrapper.module';
-export { DIRECTIVES } from './lib'
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { applyPolyfills, defineCustomElements } from '../loader';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
-
-applyPolyfills().then(() => {
-  defineCustomElements(window);
-});
